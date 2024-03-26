85806613046f1fd58d4d0afcc080e2aea8a0257f
garlam <garlam_au@qq.com>
2024-03-26 11:41:57 +0800

    update

---
+++ alter_schema.sql
@@ -0,0 +1,11 @@
+
+
+create schema ace_cloud default character set utf8 collate utf8_general_ci;#创建数据库
+grant select,insert,update,delete,create on ace_cloud.* to root;#用户授权数据库
+flush privileges;#立即启用配置
+
+
+grant select,insert,update,delete,create on ace.* to garlam;#用户授权数据库
+commit;
+drop schema ace_cloud;#删除数据库
+
