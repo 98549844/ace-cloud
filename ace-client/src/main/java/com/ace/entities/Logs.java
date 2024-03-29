dd9acfc3f87d489bea57adb5f153002126fa0549
garlam <garlam_au@qq.com>
2024-03-30 06:28:51 +0800

    update

---
+++ ace-client/src/main/java/com/ace/entities/Logs.java
@@ -0,0 +1,61 @@
+package com.ace.entities;
+
+import com.ace.entities.base.BaseEntity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "logs")
+@Entity
+public class Logs extends BaseEntity implements Serializable {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GenericGenerator(strategy = "identity", name = "id")
+    @Column(name = "logId")
+    private Long logId;
+
+    @Column(name = "operator")
+    private String operator;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "accessTime")
+    private LocalDateTime accessTime;
+
+    public Long getLogId() {
+        return logId;
+    }
+
+    public void setLogId(Long logId) {
+        this.logId = logId;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getAccessTime() {
+        return accessTime;
+    }
+
+    public void setAccessTime(LocalDateTime accessTime) {
+        this.accessTime = accessTime;
+    }
+}
