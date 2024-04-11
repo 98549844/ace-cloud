fae7828e99d94fa21118281a8b118994ff2f16d8
garlam <garlam_au@qq.com>
2024-04-12 01:17:05 +0800

    update

--- alter_schema.sql
+++ alter_schema.sql
@@ -4,6 +4,14 @@ create schema ace_cloud default character set utf8mb4 collate utf8mb4_unicode_ci
 grant select,insert,update,delete,create on ace_cloud.* to root;#用户授权数据库
 flush privileges;#立即启用配置
 
+create schema ace_client default character set utf8mb4 collate utf8mb4_unicode_ci;#创建数据库
+grant select,insert,update,delete,create on ace_client.* to root;#用户授权数据库
+flush privileges;#立即启用配置
+
+create schema ace_gateway default character set utf8mb4 collate utf8mb4_unicode_ci;#创建数据库
+grant select,insert,update,delete,create on ace_gateway.* to root;#用户授权数据库
+flush privileges;#立即启用配置
+
 
 grant select,insert,update,delete,create on ace.* to garlam;#用户授权数据库
 commit;
