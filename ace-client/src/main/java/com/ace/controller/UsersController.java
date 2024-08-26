4f8f17567c156391ea762b130e8568bad5d0f268
garlam <garlam_au@qq.com>
2024-08-26 23:50:49 +0800

    update

--- ace-client/src/main/java/com/ace/controller/UsersController.java
+++ ace-client/src/main/java/com/ace/controller/UsersController.java
@@ -28,41 +28,58 @@ public class UsersController {
     @Resource
     private UsersApi usersApi;
 
+    /**
+     * 生成默认用户
+     *
+     * @return
+     */
+    @Operation(summary = "生成默认用户")
+    @GetMapping(value = "/userGenerator")
+    public RespData userGenerator() {
+        log.info("生成默认用户");
+        RespData respData = usersApi.userGenerator();
+        return respData;
+    }
 
-    /** 返回所有用户
+    /**
+     * 返回所有用户
+     *
      * @return
      */
     @Operation(summary = "获取所有用户")
     @GetMapping(value = "/getAll")
-    public RespData getAll(){
+    public RespData getAll() {
+        log.info("获取所有用户");
         RespData respData = usersApi.getAll();
-
         return respData;
     }
 
-    /** 根据userAccount获取用户资料
+    /**
+     * 根据userAccount获取用户资料
+     *
      * @param userAccount
      * @return
      */
     @Operation(summary = "根据用户帐号获取资料")
     @GetMapping(value = "/get/{userAccount}")
-    public RespData getByUserAccount(@PathVariable(value = "userAccount") String userAccount){
+    public RespData getByUserAccount(@PathVariable(value = "userAccount") String userAccount) {
+        log.info("根据用户帐号获取资料");
         RespData respData = usersApi.getByUserAccount(userAccount);
-
         return respData;
     }
 
 
-    /** 更新用户料资
+    /**
+     * 更新用户料资
+     *
      * @param userDto
      * @return
      */
     @Operation(summary = "更新用户资料")
     @PostMapping(value = "/update")
-    public RespData updateUser(@RequestBody UsersDto userDto){
-
+    public RespData updateUser(@RequestBody UsersDto userDto) {
+        log.info("更新用户资料");
         RespData respData = usersApi.updateUser(userDto);
-
         return respData;
     }
 
@@ -73,47 +90,41 @@ public class UsersController {
     @Operation(summary = "新增用户")
     @PostMapping(value = "/new")
     public RespData insertUser(@RequestBody UsersDto userDto) {
+        log.info("新增用户");
         RespData respData = usersApi.insertUser(userDto);
-
         return respData;
 
     }
 
 
-    /** 删除用户资资料
+    /**
+     * 删除用户资资料
+     *
      * @param userAccount
      * @return
      */
     @Operation(summary = "删除用户")
     @GetMapping(value = "/delete/{userAccount}")
-    public RespData deleteByUserAccount(@PathVariable(value = "userAccount") String userAccount){
-
+    public RespData deleteByUserAccount(@PathVariable(value = "userAccount") String userAccount) {
+        log.info("删除用户");
         RespData respData = usersApi.deleteByUserAccount(userAccount);
-
         return respData;
     }
 
-    /** 删除所有用户资料
+    /**
+     * 删除所有用户资料
+     *
      * @return
      */
     @Operation(summary = "删除所有用户")
     @GetMapping(value = "/delete/all")
     public RespData deleteAll() {
+        log.info("删除所有用户");
         RespData respData = usersApi.deleteAll();
-
         return respData;
     }
 
-    /** 生成默认用户
-     * @return
-     */
-    @Operation(summary = "生成默认用户")
-    @GetMapping(value = "/userGenerator")
-    public RespData userGenerator() {
-        RespData respData = usersApi.userGenerator();
 
-        return respData;
-    }
 
 
 }
