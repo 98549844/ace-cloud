d99179699752d3fa8a23f90853fb029024d8b168
garlam <garlam_au@qq.com>
2024-11-20 10:37:33 +0800

    update

---
+++ ace-utilities/src/main/java/com/ace/AceUtilities.java
@@ -0,0 +1,20 @@
+package com.ace;
+
+
+/**
+ * @Classname: ${NAME}
+ * @Date: 21/3/2024 5:08 pm
+ * @Author: garlam
+ * @Description:
+ */
+
+//https://logging.apache.org/log4j/2.x/manual/configuration.html
+public class AceUtilities {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
