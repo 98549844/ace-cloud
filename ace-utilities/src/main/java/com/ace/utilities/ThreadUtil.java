a245af01bba31dfd003d88d4adc938990debe57c
garlam <garlam_au@qq.com>
2024-12-11 09:25:09 +0800

    update

--- ace-utilities/src/main/java/com/ace/utilities/ThreadUtil.java
+++ ace-utilities/src/main/java/com/ace/utilities/ThreadUtil.java
@@ -1,7 +1,13 @@
 package com.ace.utilities;
 
+import com.ace.utilities.datetime.LocalDateTimeUtil;
+import com.itextpdf.commons.utils.DateTimeUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.time.LocalDateTime;
+
+import static com.itextpdf.commons.utils.DateTimeUtil.getCurrentTimeDate;
 
 
 /**
@@ -17,11 +23,12 @@ public class ThreadUtil {
 
 
     public static void printStackTrace() {
+        Console.println("[ ====== start to stack trace ====== ]");
         StackTraceElement[] a = Thread.currentThread().getStackTrace();
         for (int i = 0; i < a.length; i++) {
-            System.out.println(i + ": " + a[i]);
+            System.out.println(LocalDateTimeUtil.getCurrentDatetime() + "  " + i + ": " + a[i]);
         }
+        Console.println("[ ====== end to stack trace ====== ]");
     }
-
 }
 
--- ace-utilities/src/main/java/com/ace/utilities/datetime/LocalDateTimeUtil.java
+++ ace-utilities/src/main/java/com/ace/utilities/datetime/LocalDateTimeUtil.java
@@ -27,6 +27,7 @@ import static java.time.format.DateTimeFormatter.ofPattern;
 public class LocalDateTimeUtil {
     private static final Logger log = LogManager.getLogger(LocalDateTimeUtil.class.getName());
     public static String DATETIME_PATTERN_yyyyMMddHHmmss = "yyyy-MM-dd HH:mm:ss";
+    public static String DATETIME_PATTERN_yyyyMMddHHmmssS = "yyyy-MM-dd HH:mm:ssS";
 
     // 定义 ISO 格式日期时间字符串的格式
     public static String ISO_DATE_PATTERN_yyyyMMdd = "yyyy-MM-dd'T'HH:mm:ss.SSSX";
@@ -167,7 +168,10 @@ public class LocalDateTimeUtil {
 
     public static String toLocalDateTimeString(LocalDateTime localDateTime) {
         return localDateTime.format(DateTimeFormatter.ofPattern(DATETIME_PATTERN_yyyyMMddHHmmss));
+    }
 
+    public static String getCurrentDatetime() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATETIME_PATTERN_yyyyMMddHHmmssS));
     }
 
 }
