8ffb73ced23f1d1bd6347ddc724d6de5ccccc62a
garlam <garlam_au@qq.com>
2025-02-23 21:12:25 +0800

    update

--- ace-utilities/src/main/java/com/ace/utilities/SqlUtil.java
+++ ace-utilities/src/main/java/com/ace/utilities/SqlUtil.java
@@ -34,39 +34,76 @@ public class SqlUtil {
     }
 
     @SuppressWarnings("unchecked")
-    public static Set<String> getDistinct(String target) throws IOException {
-        List<String> targetList = (List<String>) FileUtil.read(target).get(FileUtil.LIST);
+    public static Set<String> getDistinct(String path) throws IOException {
+        List<String> targetList = (List<String>) FileUtil.read(path).get(FileUtil.LIST);
         return SetUtil.toSet(targetList);
     }
 
     /**
      * 写入集合到文件
-     *
-     * @param filePath
-     * @param fileName
-     * @param set
-     * @param <T>
-     * @throws IOException
      */
-    public static <T> void writeDistinct(String filePath, String fileName, Set<T> set) throws IOException {
-        FileUtil.write(filePath, fileName, set, false);
+    public static void writeDistinct(String path, String targetPath, String fileName) throws IOException {
+        Set<String> set = getDistinct(path);
+        FileUtil.write(targetPath, fileName, set, false);
     }
 
     /**
      * 写入集合到文件, 并拼入单引号
+     */
+    public static void writeDistinctString(String path, String filePath, String fileName) throws IOException {
+        Set<String> result = getDistinctString(path);
+        FileUtil.write(filePath, fileName, result, false);
+    }
+
+
+    public static Set<String> getDistinctString(String path) throws IOException {
+        Set<String> set = getDistinct(path);
+        Set<String> result = new HashSet<>();
+        for (String s : set) {
+            if (!s.startsWith("'") || !s.endsWith("'")) {
+                s = s.replace("'", "");
+                s = "'" + s + "'";
+            }
+            if (!s.contains(",")) {
+                s = s + ",";
+            }
+            result.add(s);
+        }
+        return result;
+    }
+
+
+    /**
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    public static Set<String> getDistinctNumber(String path) throws IOException {
+        Set<String> ss = getDistinct(path);
+        Set<String> result = new HashSet<>();
+        for (String s : ss) {
+            if (s.contains("'")) {
+                s = s.replace("'", "");
+            }
+            if (!s.contains(",")) {
+                s = s + ",";
+            }
+            result.add(s);
+        }
+        return result;
+    }
+
+    /**
+     * 去除重复并写入到文件
      *
-     * @param filePath
      * @param fileName
      * @param set
      * @param <T>
      * @throws IOException
      */
-    public static <T> void writeDistinctString(String filePath, String fileName, Set<T> set) throws IOException {
-        Set<String> setString = new HashSet<>();
-        for (T t : set) {
-            setString.add("'" + t.toString() + "'");
-        }
-        FileUtil.write(filePath, fileName, setString, false);
+    public static <T> void writeDistinctNumber(String filePath, String fileName, Set<T> set) throws IOException {
+        Set<String> result = getDistinctNumber(fileName);
+        FileUtil.write(filePath, fileName, set, false);
     }
 
 
